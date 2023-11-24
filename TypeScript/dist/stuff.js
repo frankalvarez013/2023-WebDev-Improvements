8f0b0d99543697d19703acb3451785b50159a38b
Frank Alvarez <frankalvarez013@gmail.com>
2023-11-23 22:24:43 -0800

    TypeScript! NOtes

---
+++ TypeScript/dist/stuff.js
@@ -0,0 +1,6 @@
+function add(a, b) {
+    return a + b;
+}
+function addWithType(a, b) {
+    return a + b;
+}
---
+++ TypeScript/src/stuff.ts
@@ -0,0 +1,7 @@
+function add(a,b){
+    return a + b
+}
+
+function addWithType(a:number,b:number): number{
+    return a + b
+}
